543a3a7bafc6c4cfe6b01fce6ca0a4b399287320
Larry Scott <iamjustfresh@gmail.com>
2017-05-15 17:00:49 -0400

    fixed cocktails MOVED DOWN

--- app/models/actcode.rb
+++ app/models/actcode.rb
@@ -1,10 +1,10 @@
 class Actcode < ApplicationRecord
    belongs_to :management
-   
+
    enum status: [:notactive, :active]
-   
-      scope :getallbycompany, lambda { |acts| where("management_id = ?", acts.management_id).includes(:management) }
-   
+
+      scope :getallbycompany, lambda { |acts| where("management_id = ?", acts.management_id).includes(:management)} #|| lambda {|acts| where("management_id = ?", acts.roles_mask )}
+      #scope :getallbycompany, lambda { |acts| where("management_id = ?", acts.roles_mask)}
+
       scope :mainacts,  -> { where(:management_id => [1,2,3,4,5,8,16,13])}
 end
-
