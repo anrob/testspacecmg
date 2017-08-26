b1788cd0602a86b7309aa97b28b405150eab77f3
CMG V3 <iamjustfresh@gmail.com>
2017-08-26 02:11:34 +0000

    new json

--- app/controllers/api/v1/contracts_controller.rb
+++ app/controllers/api/v1/contracts_controller.rb
@@ -2,11 +2,13 @@ module Api
    module V1
       class ContractsController < ActionController::Base
          
+         respond_to :json
+         
          
          def index
-            #respond_with Contract.default.where(act_code: params[:act_code])
-            @contract = Contract.default.where(act_code: params[:act_code])
-            render json: @contract
+          respond_with Contract.default.where(act_code: params[:act_code]).as_json(root: true)
+          # @b = ActiveSupport::JSON.decode(@h)
+          #@@h
          end
          
       end
